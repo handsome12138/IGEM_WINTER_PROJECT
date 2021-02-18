d692e29e4f5bc9b8b24956cc7b4b6293fdea3364
handsome12138 <53861583+handsome12138@users.noreply.github.com>
2021-02-18 13:21:32 +0800

    daily

--- web/myweb2/src/views/Model.vue
+++ web/myweb2/src/views/Model.vue
@@ -1,7 +1,25 @@
 <template>
-    <div>
-        Model
-        <div v-katex:auto>\({{mylatex}}\)</div>
+    <div class="model" style="text-align:left">
+        <div class="model-body">
+          <h1 id="Notations"> Notations </h1>
+          <div class="model-notations">
+            Here are some conventions about notation.
+            <ol style="text-align:left">
+              <li>words begin with <span v-katex:auto>\(g\)</span> represent gene(eg., <span v-katex:auto>\(gSos\)</span>).</li>
+              <li>words begin with <span v-katex:auto>\(m\)</span> represent mRNA(eg., <span v-katex:auto>\(mSos\)</span>).</li>
+              <li>words begin with <span v-katex:auto>\(p\)</span> represent promoter(eg., <span v-katex:auto>\(pSos\)</span>).</li>
+              <li>other words represent protein(eg., Sos).</li>
+              <li><span v-katex:auto>\(\alpha\)</span> represents transcription term(eg., <span v-katex:auto>\(\alpha_{Sos}\)</span>).</li>
+              <li><span v-katex:auto>\(\beta\)</span> represents translation term(eg., <span v-katex:auto>\(\beta_{Sos}\)</span>).</li>
+              <li><span v-katex:auto>\(d\)</span> represents decomposition term(eg., <span v-katex:auto>\(d_{Sos}\)</span>).</li>
+            </ol>
+          </div>
+
+          <h1 id="basic"> Basic View of Our Model </h1>
+          <div class="model-basic">
+            Here is basic model view.
+          </div>
+        </div>
     </div>
 </template>
 
@@ -19,11 +37,17 @@ Vue.use(VueKatex, {
 export default {
   data () {
     return {
-      mylatex: 'a = \\sqrt{3}'
+      mylatex: "test latex a = \sqrt{3}"
     }
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.model-body{
+  margin-left:100px;
+  h1{
+    text-align: center;
+  }
+}
 </style>
