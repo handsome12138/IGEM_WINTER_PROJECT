44611afe4d76dd7b2e5d19cc7dc55956b3f00488
JI-9-Z <68212260+JI-9-Z@users.noreply.github.com>
2021-02-19 16:18:00 +0800

    Update Home.vue

--- web/myweb2/src/views/Home.vue
+++ web/myweb2/src/views/Home.vue
@@ -1,5 +1,27 @@
 <template>
     <div>
-        home
+        <img :src="load" id="load" />
     </div>
 </template>
+
+<style>
+
+#load{
+	height: 10%;
+}
+
+</style>
+
+<script>
+
+import loading from '../assets/home/loading.gif'
+
+export default {
+  data: function(){
+    return{
+    	load: loading
+    }
+  }
+}
+
+</script>
