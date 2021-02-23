a554104121a724cd2646e579dd801dfd11d5ad3a
JI-9-Z <68212260+JI-9-Z@users.noreply.github.com>
2021-02-23 20:35:13 +0800

    Update Side_nav.vue

--- web/myweb2/src/components/Side_nav.vue
+++ web/myweb2/src/components/Side_nav.vue
@@ -1,9 +1,13 @@
-<template>
+template>
     <div class="mynav" id="mynav">
         <ul class="leftnav">
             <li v-for="nav in navs" class="nav-item" v-bind:key="nav.name">
                 <!-- <a class="nav-link" :href="'#' + nav.id">{{nav.name}}</a> -->
-                <a class="nav-link" @click="jump_anchor(nav.id)">{{nav.name}}</a>
+                <div class="rail">
+                    <img :src="side" style="width: 20%;" />
+                    <a class="nav-link" @click="jump_anchor(nav.id)">{{nav.name}}</a>
+                </div>            
+                
             </li>
         </ul>
         <!-- a标签会使得浏览器url变化 -->
@@ -16,17 +20,24 @@
 <style lang="scss" scoped>
 .mynav{
 	flex: 0 0 20%;
+    -ms-flex: 0 0 20%;
+    -moz-flex: 0 0 20%;/* Firefox */
+    -webkit-flex: 0 0 20%; /* Safari and Chrome */
+    -o-flex: 0 0 20%; /* Opera */
     .leftnav{
         font-family: 'Sansita',sans-serif;
         list-style-type: none;
         position:fixed;
-        padding-top: 5%;
-        padding-left: 3%;
+        padding-top: 2%;
+        padding-left: 2%;
         a{
             text-align: left;
-            font-size: 1.2rem;
+            font-size: 1rem;
             color: #767676;
             cursor: pointer;
+            position: absolute;
+            top: 30%;
+            left: 0.8%;
             &:link, &:visited{
                 color:black
             }
@@ -49,16 +60,22 @@
     //     }
     // }
 }
+
+.rail{
+    position: relative;
+}
 </style>
 
 <script>
+import side from '../assets/side.png'
+
 export default {
   props: [
     'navs'
   ],
   data () {
     return {
-
+        side: side
     }
   },
   methods: {
