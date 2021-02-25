c89395e79268ee1dc65966b2e8d7e448d815f0b2
JI-9-Z <68212260+JI-9-Z@users.noreply.github.com>
2021-02-25 20:31:14 +0800

    Update Home.vue

--- web/myweb2/src/views/Home.vue
+++ web/myweb2/src/views/Home.vue
@@ -1,29 +1,28 @@
 <template>
     <div>
-    	<div id=theme>
+    	<div id='theme'>
     		<img :src="pathway" id="pathway" />
     		<img :src="mount" id="mount" />
     	</div>
         <router-link to='/Description' class="rout1"><img :src="cell1" id="cell1" :style="{'animation-play-state': state}" /></router-link>
-        <router-link to='/Modeling'><img :src="cell2" id="cell2" :style="{'animation-play-state': state}" /></router-link>
+        <router-link to='/Model'><img :src="cell2" id="cell2" :style="{'animation-play-state': state}" /></router-link>
         <router-link to='/Team'><img :src="cell3" id="cell3" :style="{'animation-play-state': state}" /></router-link>
         <router-link to='/HP'><img :src="cell4" id="cell4" :style="{'animation-play-state': state}" /></router-link>
 
         <router-link to='/Description'><img :src='chat1' id='chat1' v-show="show1" :style="{'animation-play-state': state1}"/></router-link>
         <router-link to='/Team'><img :src='chat2' id='chat2' v-show="show1"  :style="{'animation-play-state': state1}" /></router-link>
-        <router-link to='/Modeling'><img :src='chat3' id='chat3' v-show="show1" :style="{'animation-play-state': state1}" /></router-link>
+        <router-link to='/Model'><img :src='chat3' id='chat3' v-show="show1" :style="{'animation-play-state': state1}" /></router-link>
         <router-link to='/HP'><img :src='chat4' id='chat4' v-show="show1" :style="{'animation-play-state': state1}" /></router-link>
         
-        
-        
-        
-        
+             
        
         <div>
         	<transition>
         	<img :src="train" v-show="show" id="toytrain" name="toy" :style="{'animation-play-state': state}" />
         	</transition>
         </div>
+
+        
         
     </div>
 </template>
@@ -45,10 +44,10 @@
 	position: absolute;
 	left: 18%;
 	top:105%;
-	animation:cellone 1s  10s forwards;
-	-moz-animation:cellone 1s  10s forwards;/* Firefox */
-	-webkit-animation:cellone 1s  10s forwards; /* Safari and Chrome */
-	-o-animation:cellone 1s  10s forwards; /* Opera */
+	animation:cellone 1s  6s forwards;
+	-moz-animation:cellone 1s  6s forwards;/* Firefox */
+	-webkit-animation:cellone 1s  6s forwards; /* Safari and Chrome */
+	-o-animation:cellone 1s  6s forwards; /* Opera */
 }
 
 @keyframes cellone
@@ -86,10 +85,10 @@
 	height: 50%;
 	left: 35%;
 	top:105%;	
-	animation:celltwo 1s  11s forwards;
-	-moz-animation:celltwo 1s  11s forwards;/* Firefox */
-	-webkit-animation:celltwo 1s  11s forwards; /* Safari and Chrome */
-	-o-animation:celltwo 1s  11s forwards; /* Opera */
+	animation:celltwo 1s  7s forwards;
+	-moz-animation:celltwo 1s  7s forwards;/* Firefox */
+	-webkit-animation:celltwo 1s  7s forwards; /* Safari and Chrome */
+	-o-animation:celltwo 1s  7s forwards; /* Opera */
 }
 
 @keyframes celltwo
@@ -128,10 +127,10 @@
 	height: 55%;
 	left: 50%;
 	top:104%;
-	animation:cellth 1s  12s forwards;
-	-moz-animation:cellth 1s  12s forwards;/* Firefox */
-	-webkit-animation:cellth 1s  12s forwards; /* Safari and Chrome */
-	-o-animation:cellth 1s  12s forwards; /* Opera */
+	animation:cellth 1s  8s forwards;
+	-moz-animation:cellth 1s  8s forwards;/* Firefox */
+	-webkit-animation:cellth 1s  8s forwards; /* Safari and Chrome */
+	-o-animation:cellth 1s  8s forwards; /* Opera */
 }
 
 @keyframes cellth
@@ -169,10 +168,10 @@
 	height: 55%;
 	left: 61%;
 	top:107%;
-	animation:cellfour 1s  13s forwards;
-	-moz-animation:cellfour 1s  13s forwards;/* Firefox */
-	-webkit-animation:cellfour 1s  13s forwards; /* Safari and Chrome */
-	-o-animation:cellfour 1s  13s forwards; /* Opera */
+	animation:cellfour 1s  9s forwards;
+	-moz-animation:cellfour 1s  9s forwards;/* Firefox */
+	-webkit-animation:cellfour 1s  9s forwards; /* Safari and Chrome */
+	-o-animation:cellfour 1s  9s forwards; /* Opera */
 }
 
 @keyframes cellfour
@@ -218,10 +217,10 @@
 	position: absolute;
 	top:25%;
 	left: 90%;
-	animation:mymove 8s 1s forwards;
-	-moz-animation:mymove 8s 1s forwards; /* Firefox */
-	-webkit-animation:mymove 8s 1s forwards; /* Safari and Chrome */
-	-o-animation:mymove 8s 1s forwards; /* Opera */ 	
+	animation:mymove 6s forwards;
+	-moz-animation:mymove 6s forwards; /* Firefox */
+	-webkit-animation:mymove 6s forwards; /* Safari and Chrome */
+	-o-animation:mymove 6s forwards; /* Opera */ 	
 }
 
 
@@ -406,7 +405,6 @@
 
 <script>
 
-import loading from '../assets/home/loading.gif'
 import train from '../assets/home/train.png'
 import pathway from '../assets/home/pathway.png'
 import cell1 from '../assets/home/cell1.png'
@@ -423,7 +421,6 @@ import chat4 from '../assets/home/chat4.png'
 export default {
   data: function(){
     return{
-    	load: loading,
     	train: train,
     	pathway: pathway,
     	cell1: cell1,
@@ -446,6 +443,7 @@ export default {
 
   mounted(){
   		window.addEventListener('scroll', this.Scroll, true)
+  		
 	},
 
   methods:{
@@ -459,13 +457,9 @@ export default {
         setTimeout(function() {
         	that.show1 = true
         	that.state1 = 'running'
-        }, 14000);
+        }, 10000);
       } 
-    },
-
-  	/*enter(){
-  		this.show = !this.show		
-  	}*/
+    }
   }
 
 }
